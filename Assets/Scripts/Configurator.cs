abb6eb22d8a0cf69eea405ef064759b24c5512dd
Totokis <pawelj_janusz@wp.pl>
2021-07-09 12:43:45 +0200

    Commit with PlayFabSDK, contains errors, beacause there is no needed mulitplayer agent files

--- Assets/Scripts/Configurator.cs
+++ Assets/Scripts/Configurator.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using PlayFab;
+using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
 
 public class Configurator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] bool isRemote = false;
+   List<ConnectedPlayer> _connectedPlayers;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Awake()
+   {
+      if (isRemote)
+      {
+         StartRemoteServer();
+      }
+   }
+   private void StartRemoteServer()
+   {
+      Debug.Log("[ServerStartUp].StartRemoteServer");
+      _connectedPlayers = new List<ConnectedPlayer>();
+      PlayFabMultiplayerAgentAPI.Start();
+      PlayFabMultiplayerAgentAPI.IsDebugging = configuration.playFabDebugging;
+      PlayFabMultiplayerAgentAPI.OnMaintenanceCallback += OnMaintenance;
+      PlayFabMultiplayerAgentAPI.OnShutDownCallback += OnShutdown;
+      PlayFabMultiplayerAgentAPI.OnServerActiveCallback += OnServerActive;
+      PlayFabMultiplayerAgentAPI.OnAgentErrorCallback += OnAgentError;
+   }
+
 }
